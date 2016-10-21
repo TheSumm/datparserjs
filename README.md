d079ce8cd414929e6e30caaf35cabba64cc1f1b2
Sam <summdota@gmail.com>
2016-10-21 14:43:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # datparserjs
 A parser for Tibia 11's .dat format written for node.js.
+
+## How to run
+- Clone this repository
+- Run `npm install`
+- Run `node app.js --file filename` (e.g. `node app.js --file Tibia.dat`)
---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const defaultFileName = 'Tibia.dat';
+
+const ProtoBuf = require('protobufjs'),
+		    fs = require('fs'),
+		  argv = require('minimist')(process.argv.slice(2),
+					{ alias: { 'file': 'f' },
+					  default: { 'file': defaultFileName } });
+
+const ProtoDatFile = ProtoBuf.loadProtoFile('./dat.proto').build('DatFile');
+
+console.log(`>> Loading ${argv.file}..`);
+fs.readFile(`./${argv.file}`, (err, content) => {
+	if (err) {
+		console.log(`>> Error: Unable to load ${argv.file}..`)
+		return;
+	}
+
+	console.log(`>> Decoding ${argv.file}..`);
+	const data = ProtoDatFile.decode(content, content.length);
+
+	for (const object of data.objects) {
+		if (object.attributes.marketData !== null)
+			console.log(object.clientID, object.attributes.marketData.name);
+	}
+});
+
